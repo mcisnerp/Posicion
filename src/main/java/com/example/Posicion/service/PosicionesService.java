5418a35c38bfcea282af06e37f81783f5659f025
Miguel de Jesus Cisneros Pallares <mcisnerp@everis.com>
2021-11-29 22:18:29 -0600

    Conectado a prestamos

--- src/main/java/com/example/Posicion/service/PosicionesService.java
+++ src/main/java/com/example/Posicion/service/PosicionesService.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,29 +20,30 @@ public class PosicionesService {
 
 
     public List<Cuenta> getCuentas(String userName) {
-        ResponseEntity<Cuenta[]> cuentaResponseEntity = restTemplate.getForEntity("http://localhost:8081/cuenta/buscarPorUsuario/" + userName, Cuenta[].class);
+        ResponseEntity<Cuenta[]> cuentaResponseEntity = restTemplate.getForEntity("https://cuentasequipod.herokuapp.com/cuenta/buscarPorUsuario/" + userName, Cuenta[].class);
         Cuenta[] cuenta = cuentaResponseEntity.getBody();
         List<Cuenta> cuentas = Arrays.asList(cuenta);
         return cuentas;
     }
 
 
-    public List<String> getPrestamos(Integer numeroCuenta) {
-        ResponseEntity<String[]> prestamoResponseEntity = restTemplate.getForEntity("http://localhost:8083/prestamos/prestamos-por-cuenta/" + numeroCuenta, String[].class);
-        String[] prestamo = prestamoResponseEntity.getBody();
-        List<String> prestamos = Arrays.asList(prestamo);
+    public String getPrestamos(Integer numeroCuenta) {
+        ResponseEntity<String> prestamoResponseEntity = restTemplate.getForEntity("https://equipod-prestamos.herokuapp.com/prestamos/prestamos-por-cuenta/" + numeroCuenta, String.class);
+        String prestamo = prestamoResponseEntity.getBody();
+        String prestamos = prestamo;
+        //System.out.println(prestamoResponseEntity.getStatusCode().toString());
         return prestamos;
     }
 
     public List<Usuario> getUsuarios() {
-        ResponseEntity<Usuario[]> usuarioResponseEntity = restTemplate.getForEntity("http://localhost:8080/api/auth/", Usuario[].class);
+        ResponseEntity<Usuario[]> usuarioResponseEntity = restTemplate.getForEntity("https://equipod-onboarding.herokuapp.com/api/auth/", Usuario[].class);
         Usuario[] usuario = usuarioResponseEntity.getBody();
         List<Usuario> usuarios = Arrays.asList(usuario);
         return usuarios;
     }
 
     public List<PlazoFijo> getPlazoFijos(Integer numeroCuenta) {
-        ResponseEntity<PlazoFijo[]> plazoFijoResponseEntity = restTemplate.getForEntity("http://localhost:8084/plazofijo/listadoplazofijo/" + numeroCuenta, PlazoFijo[].class);
+        ResponseEntity<PlazoFijo[]> plazoFijoResponseEntity = restTemplate.getForEntity("https://inversionesequipod.herokuapp.com/plazofijo/listadoplazofijo/" + numeroCuenta, PlazoFijo[].class);
         PlazoFijo[] plazoFijo = plazoFijoResponseEntity.getBody();
         List<PlazoFijo> plazoFijos = Arrays.asList(plazoFijo);
         //if (plazoFijoResponseEntity.getStatusCode().is5xxServerError()) {
@@ -49,21 +51,32 @@ public class PosicionesService {
         //}
         return plazoFijos;
     }
-
+/*
+    public List<Usuario> getUsuarios() {
+        ResponseEntity<Usuario[]> usuarioResponseEntity = restTemplate.getForEntity("https://equipod-onboarding.herokuapp.com/api/auth/", Usuario[].class);
+        Usuario[] usuario = usuarioResponseEntity.getBody();
+        List<Usuario> usuarios = Arrays.asList(usuario);
+        return usuarios;
+    }
+*/
     public List<PlazoFijo> getPlazosPorCuenta(Cuenta cuenta){
         return getPlazoFijos(cuenta.getNumeroCuenta());
     }
-    //listadoplazofijo
+
     public Usuario getUsuario(String userName) {
         List<Usuario> usuarios = getUsuarios();
         List<Cuenta> cuentas;
+        List<String> prestamos=new ArrayList<>();
         List<PlazoFijo> plazoFijos;
         for (Usuario u : usuarios) {
             if (u.getUsername().equals(userName)) {
                 cuentas = getCuentas(u.getUsername());
                 for (Cuenta c : cuentas) {
                     c.setPlazoFijos(getPlazosPorCuenta(c));
-                    c.setPrestamos(getPrestamos(c.getNumeroCuenta()));
+                    if(c.getNumeroCuenta()!=null) {
+                        prestamos.add(getPrestamos(c.getNumeroCuenta()));
+                    }
+                    c.setPrestamos(prestamos);
                 }
                 u.setCuentas(cuentas);
                 return u;
@@ -72,4 +85,5 @@ public class PosicionesService {
         }
         return null;
     }
+
 }
